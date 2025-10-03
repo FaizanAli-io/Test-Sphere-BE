e80018038f92d9059318f5d08c761b325dbc6738
Faizan Ali <fali.abdulali@gmail.com>
2025-10-03 19:39:30 +0500

    .env fix

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 
-  fetchTokens();
+  if (process.env.NODE_ENV === 'development') fetchTokens();
 }
 bootstrap();
 
